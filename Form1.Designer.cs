aa4e5eba379483c881cbae5765f514dbc1042546
PatrycjaL <landaspatrycja@gmail.com>
2026-05-22 08:31:32 +0200

    Implement number drawing

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,11 +28,58 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        components = new System.ComponentModel.Container();
+        btnLosuj = new Button();
+        lblWynik = new Label();
+        lblWylosowaneText = new Label();
+        SuspendLayout();
+        // 
+        // btnLosuj
+        // 
+        btnLosuj.Location = new Point(934, 112);
+        btnLosuj.Name = "btnLosuj";
+        btnLosuj.Size = new Size(277, 100);
+        btnLosuj.TabIndex = 0;
+        btnLosuj.Text = "Losuj";
+        btnLosuj.UseVisualStyleBackColor = true;
+        btnLosuj.Click += btnLosuj_Click;
+        // 
+        // lblWynik
+        // 
+        lblWynik.AutoSize = true;
+        lblWynik.Font = new Font("Segoe UI", 28.125F, FontStyle.Regular, GraphicsUnit.Point, 238);
+        lblWynik.Location = new Point(66, 112);
+        lblWynik.Name = "lblWynik";
+        lblWynik.Size = new Size(535, 100);
+        lblWynik.TabIndex = 1;
+        lblWynik.Text = "Brak losowania";
+        // 
+        // lblWylosowaneText
+        // 
+        lblWylosowaneText.AutoSize = true;
+        lblWylosowaneText.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
+        lblWylosowaneText.Location = new Point(66, 55);
+        lblWylosowaneText.Name = "lblWylosowaneText";
+        lblWylosowaneText.Size = new Size(294, 45);
+        lblWylosowaneText.TabIndex = 2;
+        lblWylosowaneText.Text = "Wylosowane liczby:";
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(13F, 32F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(800, 450);
+        ClientSize = new Size(1680, 1520);
+        Controls.Add(lblWylosowaneText);
+        Controls.Add(lblWynik);
+        Controls.Add(btnLosuj);
+        Name = "Form1";
         Text = "Form1";
+        ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
+
+    private Button btnLosuj;
+    private Label lblWynik;
+    private Label lblWylosowaneText;
 }
--- Form1.cs
+++ Form1.cs
@@ -7,4 +7,26 @@ public partial class Form1 : Form
         InitializeComponent();
         Text = "Duży Lotek";
     }
+
+    private void btnLosuj_Click(object sender, EventArgs e)
+    {
+        List<int> wylosowaneLiczby = LosujLiczby();
+        lblWynik.Text = string.Join(", ", wylosowaneLiczby);
+    }
+
+    private List<int> LosujLiczby()
+    {
+        Random random = new Random();
+        List<int> liczby = new List<int>();
+        while (liczby.Count < 6)
+        {
+            int liczba = random.Next(1, 50);
+            if (!liczby.Contains(liczba))
+            {
+                liczby.Add(liczba);
+            }
+        }
+
+        return liczby;
+    }
 }
